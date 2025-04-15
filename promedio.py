7b0d6017ea583b4c311aeb12cd7330ca9b9859f3
volpi3 <elizabeth.troncoso.volpi@gmail.com>
2025-04-15 13:19:15 -0400

    python primera clase

---
+++ promedio.py
@@ -0,0 +1,17 @@
+# print ("ingrese 3 numeros para sacar su promedio ")
+# n1=int(input(" ingrese un numero "))
+# n2=int(input(" ingrese un numero "))
+# n3=int(input(" ingrese un numero "))
+# prom=(n1+n2+n3)/3
+# print(" el promedio es ", prom) 
+# if prom>= 40:
+#     print(" el alumno aprobo ")
+# else:
+#     print(" el alumno reprobo ")
+
+# ingrese 3 numeros y muestre el mayor de ellos 
+
+n1=int(input(" ingrese un numero "))
+n2=int(input(" ingrese un numero "))
+n3=int(input(" ingrese un numero "))
+
