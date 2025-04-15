17654e3d10949ba736e03c0676852c9747599831
volpi3 <elizabeth.troncoso.volpi@gmail.com>
2025-04-15 11:54:19 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,11 @@
+# #declaracion de variables
+# nombre="cony"
+# edad=20
+# # ejemplo de contatenacion 
+# print("hola", nombre,"y su edad es", edad )
+# print ("ingrese su nombre")
+# nombre=input()
+
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese un numero"))
+print("el resultado de la suma es",  n1+n2)
